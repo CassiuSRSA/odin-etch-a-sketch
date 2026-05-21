f9f2c9ba4a3bc7c2e371437e5dd1a096e30a9540
CassiuSRSA <s.brookstein@gmail.com>
2026-05-21 08:57:07 +0200

    Added block selector function

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const clearButton = document.getElementById("clear-button");
+const blocksButton = document.getElementById("blocks-button");
+
+blocksButton.addEventListener("click", () => {
+  const newSize = prompt("Enter the number of blocks per side (max 64):");
+  const size = Math.min(Math.max(parseInt(newSize), 1), 64);
+  console.log(size);
+
+  const container = document.querySelector(".container");
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.innerHTML = "";
+
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.classList.add("pixel");
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
+    container.appendChild(div);
+  }
+});
 
 clearButton.addEventListener("click", () => {
   const pixels = document.querySelectorAll(".pixel");
@@ -7,16 +27,4 @@ clearButton.addEventListener("click", () => {
   });
 });
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("pixel");
-  document.querySelector(".container").appendChild(div);
-}
 
-const pixels = document.querySelectorAll(".pixel");
-
-pixels.forEach((pixel) => {
-  pixel.addEventListener("mouseover", () => {
-    pixel.style.backgroundColor = "black";
-  });
-});
